620112c023656b94bcc2493878e60677774921f1
Vinicius Almeida <viniciusalmeida.vk1@gmail.com>
2023-04-22 17:25:20 -0300

    CRUD Menu created

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,24 +1,41 @@
 package application;
 
+import models.Employee;
+import models.Tasks;
+
+import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
+    private static Scanner sc = new Scanner(System.in);
+    private static ArrayList<Employee> employees = new ArrayList<>();
+    private static ArrayList<Tasks> tasks = new ArrayList<>();
+
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
 
+        int option = -1;
 
-        System.out.println("Teste");
+        do {
+            System.out.println("""
+             ================================================
+             =               Choose an option:              =
+             ================================================        
+             =    1 - Register an employee                  =
+             =    2 - List of registered employees          =
+             =    3 - Create a new task                     =
+             =    4 - List all created tasks                =
+             =    5 - Update a task                         =
+             =    6 - Delete a task                         =
+             =    0 - Exit program                          =
+             ================================================       
+                    """);
+            System.out.println("================================================ ");
+            System.out.print("Enter your choice:  ");
+            option = sc.nextInt();
 
-
-
-
-
-
-
-
-
+        } while(option != 0);
 
 
         sc.close();
